3a3be723fec691832ba466f72d9381d552612564
James Chen <james@ashchan.com>
2019-10-14 18:11:29 +0900

    Remove ckb-sdk-core

--- packages/neuron-wallet/src/models/lock-utils.ts
+++ packages/neuron-wallet/src/models/lock-utils.ts
@@ -1,19 +1,17 @@
-import Core from '@nervosnetwork/ckb-sdk-core'
-
-const core = new Core("http://127.0.0.1:8114")
+import { bech32Address, AddressPrefix, parseAddress, AddressType } from '@nervosnetwork/ckb-sdk-utils'
 
 export default class LockUtils {
   static blake160ToAddress(blake160: string): string {
-    const prefix = core.utils.AddressPrefix.Mainnet
-    return core.utils.bech32Address(blake160, {
+    const prefix = AddressPrefix.Mainnet
+    return bech32Address(blake160, {
       prefix,
-      type: core.utils.AddressType.HashIdx,
+      type: AddressType.HashIdx,
       codeHashIndex: '0x00',
     })
   }
 
   static addressToBlake160(address: string): string {
-    const result: string = core.utils.parseAddress(address, 'hex') as string
+    const result: string = parseAddress(address, 'hex') as string
     const hrp: string = `0100`
     let blake160: string = result.slice(hrp.length + 2, result.length)
     if (!blake160.startsWith('0x')) {
